ed068dd3cea82cd1b9c4511f062237c595d64e5f
Thuan Do <thuando.contact@gmail.com>
2025-03-07 13:59:36 +0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './thuando-portfolio/' // This ensures assets are loaded correctly on GitHub Pages
+  base: '/thuando-portfolio/' // This ensures assets are loaded correctly on GitHub Pages
 })
